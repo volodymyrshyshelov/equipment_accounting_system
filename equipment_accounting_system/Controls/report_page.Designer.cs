7f1019088aef9bc15d8aeae9418e7ca3b520cdf8
Vladimir Shyshelov <vladimir.shyshelov@gmail.com>
2024-05-20 00:35:32 +0300

    v.0.1.8 Add functional: Added report page. Added report filters. Added export report with class file_Export.

--- equipment_accounting_system/Controls/report_page.Designer.cs
+++ equipment_accounting_system/Controls/report_page.Designer.cs
@@ -28,22 +28,218 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             pnl_report_page = new Guna.UI2.WinForms.Guna2GradientPanel();
+            dgv_reports = new Guna.UI2.WinForms.Guna2DataGridView();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            btn_export = new Guna.UI2.WinForms.Guna2TileButton();
+            cmb_type_3 = new Guna.UI2.WinForms.Guna2ComboBox();
+            cmb_type_2 = new Guna.UI2.WinForms.Guna2ComboBox();
+            cmb_type_1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            pnl_report_page.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgv_reports).BeginInit();
+            guna2Panel1.SuspendLayout();
             SuspendLayout();
             // 
             // pnl_report_page
             // 
-            pnl_report_page.CustomizableEdges = customizableEdges1;
+            pnl_report_page.Controls.Add(dgv_reports);
+            pnl_report_page.Controls.Add(guna2Panel1);
+            pnl_report_page.CustomizableEdges = customizableEdges11;
             pnl_report_page.Dock = DockStyle.Fill;
             pnl_report_page.FillColor = Color.FromArgb(183, 54, 101);
             pnl_report_page.FillColor2 = Color.FromArgb(29, 38, 113);
             pnl_report_page.Location = new Point(0, 0);
             pnl_report_page.Name = "pnl_report_page";
-            pnl_report_page.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            pnl_report_page.ShadowDecoration.CustomizableEdges = customizableEdges12;
             pnl_report_page.Size = new Size(1008, 546);
             pnl_report_page.TabIndex = 1;
+            // 
+            // dgv_reports
+            // 
+            dgv_reports.AllowUserToAddRows = false;
+            dgv_reports.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = Color.White;
+            dataGridViewCellStyle1.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(175, 53, 102);
+            dataGridViewCellStyle1.SelectionForeColor = Color.White;
+            dgv_reports.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dgv_reports.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgv_reports.BackgroundColor = Color.FromArgb(72, 43, 110);
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = Color.FromArgb(72, 43, 110);
+            dataGridViewCellStyle2.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(72, 43, 110);
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dgv_reports.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dgv_reports.ColumnHeadersHeight = 30;
+            dgv_reports.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = Color.FromArgb(100, 46, 108);
+            dataGridViewCellStyle3.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(175, 53, 102);
+            dataGridViewCellStyle3.SelectionForeColor = Color.White;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
+            dgv_reports.DefaultCellStyle = dataGridViewCellStyle3;
+            dgv_reports.Dock = DockStyle.Fill;
+            dgv_reports.EditMode = DataGridViewEditMode.EditOnF2;
+            dgv_reports.GridColor = Color.FromArgb(231, 229, 255);
+            dgv_reports.Location = new Point(0, 50);
+            dgv_reports.MultiSelect = false;
+            dgv_reports.Name = "dgv_reports";
+            dgv_reports.ReadOnly = true;
+            dgv_reports.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = Color.FromArgb(100, 46, 108);
+            dataGridViewCellStyle4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = Color.FromArgb(100, 46, 108);
+            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
+            dgv_reports.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dgv_reports.RowHeadersVisible = false;
+            dgv_reports.RowTemplate.Height = 25;
+            dgv_reports.Size = new Size(1008, 496);
+            dgv_reports.TabIndex = 5;
+            dgv_reports.ThemeStyle.AlternatingRowsStyle.BackColor = Color.FromArgb(175, 53, 102);
+            dgv_reports.ThemeStyle.AlternatingRowsStyle.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            dgv_reports.ThemeStyle.AlternatingRowsStyle.ForeColor = Color.White;
+            dgv_reports.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = Color.FromArgb(175, 53, 102);
+            dgv_reports.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = Color.White;
+            dgv_reports.ThemeStyle.BackColor = Color.FromArgb(72, 43, 110);
+            dgv_reports.ThemeStyle.GridColor = Color.FromArgb(231, 229, 255);
+            dgv_reports.ThemeStyle.HeaderStyle.BackColor = Color.FromArgb(72, 43, 110);
+            dgv_reports.ThemeStyle.HeaderStyle.BorderStyle = DataGridViewHeaderBorderStyle.None;
+            dgv_reports.ThemeStyle.HeaderStyle.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            dgv_reports.ThemeStyle.HeaderStyle.ForeColor = Color.White;
+            dgv_reports.ThemeStyle.HeaderStyle.HeaightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dgv_reports.ThemeStyle.HeaderStyle.Height = 30;
+            dgv_reports.ThemeStyle.ReadOnly = true;
+            dgv_reports.ThemeStyle.RowsStyle.BackColor = Color.FromArgb(100, 46, 108);
+            dgv_reports.ThemeStyle.RowsStyle.BorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dgv_reports.ThemeStyle.RowsStyle.Font = new Font("Century Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            dgv_reports.ThemeStyle.RowsStyle.ForeColor = Color.White;
+            dgv_reports.ThemeStyle.RowsStyle.Height = 25;
+            dgv_reports.ThemeStyle.RowsStyle.SelectionBackColor = Color.FromArgb(175, 53, 102);
+            dgv_reports.ThemeStyle.RowsStyle.SelectionForeColor = Color.White;
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.BackColor = Color.Transparent;
+            guna2Panel1.Controls.Add(btn_export);
+            guna2Panel1.Controls.Add(cmb_type_3);
+            guna2Panel1.Controls.Add(cmb_type_2);
+            guna2Panel1.Controls.Add(cmb_type_1);
+            guna2Panel1.CustomizableEdges = customizableEdges9;
+            guna2Panel1.Dock = DockStyle.Top;
+            guna2Panel1.Location = new Point(0, 0);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2Panel1.Size = new Size(1008, 50);
+            guna2Panel1.TabIndex = 4;
+            // 
+            // btn_export
+            // 
+            btn_export.BackColor = Color.Transparent;
+            btn_export.BorderColor = Color.White;
+            btn_export.BorderRadius = 20;
+            btn_export.BorderThickness = 1;
+            btn_export.CustomBorderColor = Color.White;
+            btn_export.CustomizableEdges = customizableEdges1;
+            btn_export.DisabledState.BorderColor = Color.DarkGray;
+            btn_export.DisabledState.CustomBorderColor = Color.DarkGray;
+            btn_export.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btn_export.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btn_export.FillColor = Color.Transparent;
+            btn_export.Font = new Font("Century Gothic", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_export.ForeColor = Color.White;
+            btn_export.Location = new Point(759, 8);
+            btn_export.Name = "btn_export";
+            btn_export.PressedColor = Color.FromArgb(43, 39, 112);
+            btn_export.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            btn_export.Size = new Size(243, 35);
+            btn_export.TabIndex = 68;
+            btn_export.Text = "Експортувати";
+            btn_export.Click += btn_export_Click;
+            // 
+            // cmb_type_3
+            // 
+            cmb_type_3.BackColor = Color.Transparent;
+            cmb_type_3.BorderRadius = 20;
+            cmb_type_3.CustomizableEdges = customizableEdges3;
+            cmb_type_3.DrawMode = DrawMode.OwnerDrawFixed;
+            cmb_type_3.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmb_type_3.FillColor = Color.FromArgb(35, 39, 112);
+            cmb_type_3.FocusedColor = Color.FromArgb(94, 148, 255);
+            cmb_type_3.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            cmb_type_3.Font = new Font("Century Gothic", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            cmb_type_3.ForeColor = Color.White;
+            cmb_type_3.ItemHeight = 30;
+            cmb_type_3.Location = new Point(510, 8);
+            cmb_type_3.Name = "cmb_type_3";
+            cmb_type_3.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            cmb_type_3.Size = new Size(243, 36);
+            cmb_type_3.TabIndex = 52;
+            cmb_type_3.SelectedIndexChanged += cmb_type_3_SelectedIndexChanged;
+            // 
+            // cmb_type_2
+            // 
+            cmb_type_2.BackColor = Color.Transparent;
+            cmb_type_2.BorderRadius = 20;
+            cmb_type_2.CustomizableEdges = customizableEdges5;
+            cmb_type_2.DrawMode = DrawMode.OwnerDrawFixed;
+            cmb_type_2.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmb_type_2.FillColor = Color.FromArgb(35, 39, 112);
+            cmb_type_2.FocusedColor = Color.FromArgb(94, 148, 255);
+            cmb_type_2.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            cmb_type_2.Font = new Font("Century Gothic", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            cmb_type_2.ForeColor = Color.White;
+            cmb_type_2.ItemHeight = 30;
+            cmb_type_2.Location = new Point(261, 8);
+            cmb_type_2.Name = "cmb_type_2";
+            cmb_type_2.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            cmb_type_2.Size = new Size(243, 36);
+            cmb_type_2.TabIndex = 51;
+            cmb_type_2.SelectedIndexChanged += cmb_type_2_SelectedIndexChanged;
+            // 
+            // cmb_type_1
+            // 
+            cmb_type_1.BackColor = Color.Transparent;
+            cmb_type_1.BorderRadius = 20;
+            cmb_type_1.CustomizableEdges = customizableEdges7;
+            cmb_type_1.DrawMode = DrawMode.OwnerDrawFixed;
+            cmb_type_1.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmb_type_1.FillColor = Color.FromArgb(35, 39, 112);
+            cmb_type_1.FocusedColor = Color.FromArgb(94, 148, 255);
+            cmb_type_1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            cmb_type_1.Font = new Font("Century Gothic", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            cmb_type_1.ForeColor = Color.White;
+            cmb_type_1.ItemHeight = 30;
+            cmb_type_1.Location = new Point(12, 8);
+            cmb_type_1.Name = "cmb_type_1";
+            cmb_type_1.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            cmb_type_1.Size = new Size(243, 36);
+            cmb_type_1.TabIndex = 50;
+            cmb_type_1.SelectedIndexChanged += cmb_type_1_SelectedIndexChanged;
             // 
             // report_page
             // 
@@ -52,11 +248,20 @@
             Controls.Add(pnl_report_page);
             Name = "report_page";
             Size = new Size(1008, 546);
+            pnl_report_page.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgv_reports).EndInit();
+            guna2Panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Guna.UI2.WinForms.Guna2GradientPanel pnl_report_page;
+        private Guna.UI2.WinForms.Guna2DataGridView dgv_reports;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2TileButton btn_export;
+        private Guna.UI2.WinForms.Guna2ComboBox cmb_type_3;
+        private Guna.UI2.WinForms.Guna2ComboBox cmb_type_2;
+        private Guna.UI2.WinForms.Guna2ComboBox cmb_type_1;
     }
 }
--- equipment_accounting_system/Controls/report_page.cs
+++ equipment_accounting_system/Controls/report_page.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using equipment_accounting_system.Classes;
+using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -12,9 +15,262 @@ namespace equipment_accounting_system.Controls
 {
     public partial class report_page : UserControl
     {
+        private readonly file_Export fileExport;
         public report_page()
         {
             InitializeComponent();
+            fileExport = new file_Export();
+            LoadReportTypes();
+            cmb_type_1.SelectedIndexChanged += cmb_type_SelectedIndexChanged;
+            cmb_type_2.SelectedIndexChanged += cmb_type_2_SelectedIndexChanged;
+            cmb_type_3.SelectedIndexChanged += cmb_type_3_SelectedIndexChanged;
         }
+        private void cmb_type_1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void cmb_type_2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void cmb_type_3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadReportData();
+        }
+        private void btn_export_Click(object sender, EventArgs e)
+        {
+            if (dgv_reports.DataSource == null)
+            {
+                MessageBox.Show("Спочатку завантажте дані для експорту.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx|PDF files (*.pdf)|*.pdf|JSON files (*.json)|*.json|CSV files (*.csv)|*.csv";
+                saveFileDialog.Title = "Збереження звіту";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    string extension = System.IO.Path.GetExtension(filePath).ToLower();
+
+                    try
+                    {
+                        switch (extension)
+                        {
+                            case ".xlsx":
+                                fileExport.ExportToExcel(dgv_reports, filePath);
+                                break;
+                            case ".pdf":
+                                fileExport.ExportToPdf(dgv_reports, filePath);
+                                break;
+                            case ".json":
+                                fileExport.ExportToJson(dgv_reports, filePath);
+                                break;
+                            case ".csv":
+                                fileExport.ExportToCsv(dgv_reports, filePath);
+                                break;
+                            default:
+                                throw new Exception("Непідтримуваний формат файлу.");
+                        }
+
+                        MessageBox.Show("Дані успішно експортовані!", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Помилка експорту даних: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+
+        private void LoadReportTypes()
+        {
+            cmb_type_1.Items.AddRange(new string[] { "Отчеты по оборудованию", "Отчеты по задачам" });
+        }
+
+        private void cmb_type_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmb_type_2.Items.Clear();
+            cmb_type_3.Items.Clear();
+            dgv_reports.DataSource = null;
+
+            if (cmb_type_1.SelectedItem.ToString() == "Отчеты по оборудованию")
+            {
+                LoadEquipmentFilters();
+            }
+            else if (cmb_type_1.SelectedItem.ToString() == "Отчеты по задачам")
+            {
+                LoadTaskFilters();
+            }
+        }
+
+        private void LoadEquipmentFilters()
+        {
+            cmb_type_2.Items.AddRange(new string[] { "По типу оборудования", "По дате последнего обслуживания", "По ответственному пользователю" });
+
+            cmb_type_2.SelectedIndexChanged += (s, e) =>
+            {
+                cmb_type_3.Items.Clear();
+                if (cmb_type_2.SelectedItem.ToString() == "По типу оборудования")
+                {
+                    LoadDistinctValues("type", "equipment1", cmb_type_3, "Inventory");
+                }
+                else if (cmb_type_2.SelectedItem.ToString() == "По дате последнего обслуживания")
+                {
+                    LoadDistinctValues("lastroutinemaintenancedate", "equipment1", cmb_type_3, "Inventory");
+                }
+                else if (cmb_type_2.SelectedItem.ToString() == "По ответственному пользователю")
+                {
+                    LoadDistinctValues("workerusername", "equipment1", cmb_type_3, "Inventory");
+                }
+            };
+        }
+
+        private void LoadTaskFilters()
+        {
+            cmb_type_2.Items.AddRange(new string[] { "По типу задачи", "По приоритету", "По статусу задачи", "По ответственному пользователю" });
+
+            cmb_type_2.SelectedIndexChanged += (s, e) =>
+            {
+                cmb_type_3.Items.Clear();
+                if (cmb_type_2.SelectedItem.ToString() == "По типу задачи")
+                {
+                    LoadDistinctValues("type", "planning", cmb_type_3, "Planning");
+                }
+                else if (cmb_type_2.SelectedItem.ToString() == "По приоритету")
+                {
+                    LoadDistinctValues("priority", "planning", cmb_type_3, "Planning");
+                }
+                else if (cmb_type_2.SelectedItem.ToString() == "По статусу задачи")
+                {
+                    LoadDistinctValues("status", "planning", cmb_type_3, "Planning");
+                }
+                else if (cmb_type_2.SelectedItem.ToString() == "По ответственному пользователю")
+                {
+                    LoadDistinctValues("workername", "planning", cmb_type_3, "Planning");
+                }
+            };
+        }
+
+        private void LoadDistinctValues(string columnName, string tableName, ComboBox comboBox, string connectionStringKey)
+        {
+            string sql = $"SELECT DISTINCT {columnName} FROM {tableName}";
+
+            using (var conn = new NpgsqlConnection(ConfigurationManager.AppSettings.Get(connectionStringKey)))
+            {
+                try
+                {
+                    conn.Open();
+                    using (var cmd = new NpgsqlCommand(sql, conn))
+                    {
+                        using (var dr = cmd.ExecuteReader())
+                        {
+                            comboBox.Items.Clear();
+                            while (dr.Read())
+                            {
+                                if (columnName == "lastroutinemaintenancedate" && dr[columnName] != DBNull.Value)
+                                {
+                                    comboBox.Items.Add(Convert.ToDateTime(dr[columnName]).ToString("yyyy-MM-dd"));
+                                }
+                                else
+                                {
+                                    comboBox.Items.Add(dr[columnName].ToString());
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Помилка завантаження значень: {ex.Message}", "Помилка завантаження фільтрів", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void LoadReportData()
+        {
+
+            string selectedType1 = cmb_type_1.SelectedItem?.ToString();
+            string selectedType2 = cmb_type_2.SelectedItem?.ToString();
+            string selectedType3 = cmb_type_3.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(selectedType1) || string.IsNullOrEmpty(selectedType2) || string.IsNullOrEmpty(selectedType3))
+            {
+                MessageBox.Show("Будь ласка, оберіть всі типи фільтрів.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string tableName = selectedType1 == "Отчеты по оборудованию" ? "equipment1" : "planning";
+            string columnName = GetColumnName(selectedType2,tableName);
+            string connectionStringKey = selectedType1 == "Отчеты по оборудованию" ? "Inventory" : "Planning";
+
+            string sql;
+            if (selectedType2 == "По дате последнего обслуживания")
+            {
+                sql = $"SELECT * FROM {tableName} WHERE {columnName} = @selectedValue::date";
+            }
+            else
+            {
+                sql = $"SELECT * FROM {tableName} WHERE {columnName} = @selectedValue";
+            }
+
+            using (var conn = new NpgsqlConnection(ConfigurationManager.AppSettings.Get(connectionStringKey)))
+            {
+                try
+                {
+                    conn.Open();
+                    using (var cmd = new NpgsqlCommand(sql, conn))
+                    {
+                        if (selectedType2 == "По дате последнего обслуживания")
+                        {
+                            cmd.Parameters.AddWithValue("@selectedValue", DateTime.Parse(selectedType3));
+                        }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@selectedValue", selectedType3);
+                        }
+
+                        using (var adapter = new NpgsqlDataAdapter(cmd))
+                        {
+                            DataTable dataTable = new DataTable();
+                            adapter.Fill(dataTable);
+                            dgv_reports.DataSource = dataTable;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Помилка завантаження даних: {ex.Message}", "Помилка завантаження звіту", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private string GetColumnName(string filterType, string tableName)
+        {
+            switch (filterType)
+            {
+                case "По типу оборудования":
+                case "По типу задачи":
+                    return "type";
+                case "По дате последнего обслуживания":
+                    return "lastroutinemaintenancedate";
+                case "По ответственному пользователю":
+                    return tableName == "equipment1" ? "workerusername" : "workername";
+                case "По приоритету":
+                    return "priority";
+                case "По статусу задачи":
+                    return "status";
+                default:
+                    throw new Exception("Невідомий тип фільтра.");
+            }
+        }
+
+
     }
 }
+   
+
+
